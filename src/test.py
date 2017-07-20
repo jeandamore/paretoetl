e878a3cb5aca7dc3c8cb2338f0b2243e927ad96a
Jean D'Amore <jdamore@thoughtworks.com>
2017-07-20 20:24:14 +1000

    Added concatenation of name

--- src/test.py
+++ src/test.py
@@ -3,4 +3,7 @@ donors = etl.fromcsv('data/donors.csv')
 print donors
 donors_base = etl.rename(donors, {'DONOR_ID':'URN', 'ADDRESS':'STREET', 'GENDER':'SEX'})
 donors_base = etl.convert(donors_base, 'SEX', {'Male':'M', 'Female':'F'})
+donors_base = etl.addfield(donors_base, 'NAME', lambda rec: rec['LAST_NAME']+', '+ rec['FIRST_NAME'], 1)
+donors_base = etl.cutout(donors_base, 'FIRST_NAME', 'LAST_NAME')
+
 print donors_base
