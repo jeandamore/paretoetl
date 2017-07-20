d758b5c19350ba0ae6c6956faaf34c8d2e7d2681
Jean D'Amore <jdamore@thoughtworks.com>
2017-07-20 22:09:36 +1000

    Added client specific config in JSON file

--- src/donors_etl.py
+++ src/donors_etl.py
@@ -4,10 +4,16 @@ import pareto_etl as petl
 def load_donors(client):
 	return petl.read_csv('../data/'+client+'/donors.csv')
 
-def transform_donors(client):
-	donors_base = petl.rename_headers(donors, {'DONOR_ID':'URN', 'ADDRESS':'STREET', 'GENDER':'SEX'})
-	donors_base = petl.map_values(donors_base, 'SEX', {'Male':'M', 'Female':'F'})
-	donors_base = petl.concat_columns(donors_base, 'NAME', 'LAST_NAME', 'FIRST_NAME')
+def load_donors_config(client):
+	return petl.read_json('../config/'+client+'/donors.json')
+
+def transform_donors(client, donors):
+	donors_config = load_donors_config(client)
+	donors_base = petl.rename_headers(donors, donors_config['headers'])
+	for mapping in donors_config['mappings']:
+		donors_base = petl.map_values(donors_base, mapping['field'], mapping['values'])
+	for concat in donors_config['concats']:
+		donors_base = petl.concat_columns(donors_base, concat['new_field'], concat['old_field_left'], concat['old_field_right'])
 	return donors_base
 
 def save_donors_base(client, table):
@@ -16,7 +22,7 @@ def save_donors_base(client, table):
 
 client = sys.argv[1]
 donors = load_donors(client)
-donors_base = transform_donors(client)
+donors_base = transform_donors(client, donors)
 save_donors_base(client, donors_base)
 
 print donors
