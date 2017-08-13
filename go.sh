1e786a4bd8babdcd37b767ee54011fa06ab440e2
Jean D'Amore <jdamore@thoughtworks.com>
2017-08-13 14:06:46 +1000

    Added bash -c in go.sh

--- go.sh
+++ go.sh
@@ -1,15 +1,15 @@
 export HOMEBREW_NO_AUTO_UPDATE=1 
 set -e
 brew install python
-python -m ensurepip
-pip install virtualenv
-virtualenv .
+bash -c "python -m ensurepip"
+bash -c "pip install virtualenv"
+bash -c "virtualenv ."
 pushd bin
 source activate
 popd
-pip install petl
-pip install numpy
+bash -c "pip install petl"
+bash -c "pip install numpy"
 pushd src
-python -m unittest pareto_etl_test
-python $1.py $2
+bash -c "python -m unittest pareto_etl_test"
+bash -c "python $1.py $2"
 popd
