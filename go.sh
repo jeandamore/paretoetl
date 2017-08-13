51e6e0e823165ae88b19a56f41c9a7761e9b7619
Jean D'Amore <jdamore@thoughtworks.com>
2017-08-13 14:22:37 +1000

    Changed go.sh to only use virtualenv if not in CI

--- go.sh
+++ go.sh
@@ -2,14 +2,16 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 set -e
 brew install python
 bash -c "python -m ensurepip"
-bash -c "pip install virtualenv"
-bash -c "virtualenv ."
-pushd bin
-source activate
-popd
-bash -c "pip install petl"
-bash -c "pip install numpy"
+if [ -z "$GO_PIPELINE_NAME" ]; then
+	pip install virtualenv
+	virtualenv .
+	pushd bin
+	source activate
+	popd
+fi
+pip install petl
+pip install numpy
 pushd src
-bash -c "python -m unittest pareto_etl_test"
-bash -c "python $1.py $2"
+python -m unittest pareto_etl_test
+python $1.py $2
 popd
